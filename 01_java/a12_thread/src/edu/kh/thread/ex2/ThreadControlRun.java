bd314495a17d3ee3e1e627a6a9f27c70da75bafb
sungkuk <dbs1128218@naver.com>
2023-08-16 17:40:21 +0900

    230816 교육내용

---
+++ 01_java/a12_thread/src/edu/kh/thread/ex2/ThreadControlRun.java
@@ -0,0 +1,26 @@
+package edu.kh.thread.ex2;
+
+public class ThreadControlRun {
+	public static void main(String[] args) {
+		
+		Thread sleepTest = new Thread(new SleepThread1());
+//		sleepTest.start();
+		
+		// 시계
+		Thread clockThread = new Thread(new MyClock());
+//		clockThread.start();
+		
+		Thread test = new Thread(new SleepThread2());
+//		test.start();
+		
+		// 인터럽트 확인하기
+		InterruptTest it = new InterruptTest();
+//		it.test();
+		
+		// 스톱워치
+		StopWatchContoller controller = new StopWatchContoller();
+		controller.watchStart();
+		
+	}
+
+}
---
+++ 01_java/a12_thread/src/edu/kh/thread/ex3/MoveHeart.java
@@ -0,0 +1,34 @@
+package edu.kh.thread.ex3;
+
+// 하트가 1칸씩 움직이게 하는 효과
+public class MoveHeart implements Runnable {
+
+	@Override
+	public void run() {
+		
+		try {
+			for (int i=0 ; i<10 ; i++) {
+				
+				// 30줄 띄우기
+				for(int a=0 ; a<30 ; a++) System.out.println();
+				
+				// i 미만까지 반복하며 한 칸씩 띄우기
+				for (int h=0 ; h<i ; h++) System.out.print(" ");
+				System.out.println("♡");
+				
+				// i 미만까지 반복하며 한 칸씩 띄우기
+				for (int s=0 ; s<i ; s++) System.out.print(" ");
+				System.out.print("☆");
+					
+				Thread.sleep(1000); // 1초 일시 정지
+			}
+			
+		}catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+		
+	}
+	
+	
+	
+}
