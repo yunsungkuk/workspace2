99bcbc5494410dcc622dab5ad3540b8a03cf7fad
DESKTOP-9TSOIKI\윤성국 <dbs1128218@naver.com>
2023-10-04 18:28:52 +0900

    숙제

---
+++ 04_WebFront/3_JavaScript/js/긴급문제.js
@@ -0,0 +1,33 @@
+// document.querySelector("[name='point']:checked").value
+
+const width = document.getElementById('width');
+const height = document.getElementById('height');
+const ness = document.getElementById('ness');
+const cor = document.getElementById('cor');
+const bcor = document.getElementById('bcor');
+const letter = document.getElementById('letter');
+
+
+btn.addEventListener('click', function () {
+
+   console.log(letter.value);
+
+
+   box.style.width = width.value + 'px';
+   box.style.height = height.value + 'px';
+   box.style.fontSize = ness.value + 'px';
+   box.style.color = cor.value;
+   box.style.backgroundColor = bcor.value;
+   box.innerText = letter.value;
+   
+   if()
+   
+
+     
+   
+   
+});
+
+
+
+
