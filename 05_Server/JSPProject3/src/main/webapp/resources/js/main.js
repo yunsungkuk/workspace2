edf6d2c725d264bf47d8a66f1e55a60595ef2393
sungkuk <dbs1128218@naver.com>
2023-10-05 17:50:33 +0900

    231005 수업내용

---
+++ 05_Server/JSPProject3/src/main/webapp/resources/js/main.js
@@ -0,0 +1,19 @@
+// btn1 요소 클릭 시
+document.getElementById("btn1").addEventListener("click", () => {
+
+   // location.href        : 현재 주소 반환
+   // location.href = 주소; : 대입된 주소로 이동(GET 방식)
+   location.href = "/jstl/basic";
+});
+
+// #btn2 요소 클릭 시
+document.querySelector("#btn2").addEventListener("click", () => {
+
+   location.href = "/jstl/condition" // GET 방식
+});
+
+// btn3 요소 클릭 시
+document.getElementById("btn3").addEventListener("click", () => {
+
+   location.href = "/jstl/loop"
+});
