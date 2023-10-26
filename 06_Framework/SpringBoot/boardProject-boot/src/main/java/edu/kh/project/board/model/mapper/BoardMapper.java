ed825b03fc712f6dbda7b96b793ad2a151c92fde
sungkuk <dbs1128218@naver.com>
2023-10-26 17:47:29 +0900

    231026 수업내용

---
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/board/model/mapper/BoardMapper.java
@@ -0,0 +1,20 @@
+package edu.kh.project.board.model.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import edu.kh.project.board.model.dto.Board;
+
+@Mapper
+public interface BoardMapper {
+
+	/** 게시글 목록 조회
+	 * @param boardCode
+	 * @return
+	 */
+	List<Board> selectBoardList(int boardCode);
+	
+	
+
+}
